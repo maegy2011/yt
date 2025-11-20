04bf2cfc2e78144a1341ea0cda73a084cae20300
maegy2011 <ma.egy2011@gmail.com>
2025-11-20 15:57:45 +0200

    20112025

--- src/components/video/index.ts
+++
@@ -1,8 +0,0 @@
-export { VideoCard } from './VideoCard'
-export type { VideoCardProps, VideoCardData } from './VideoCard'
-export { 
-  favoriteVideoToCardData, 
-  watchedVideoToCardData, 
-  simpleVideoToCardData, 
-  toVideoCardData 
-} from './videoCardConverters'
